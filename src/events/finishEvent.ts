887997ff211796cb7d0eeee3242150a38d15e114
Eduardo Ferreira Gomes <dududuefu.gomes@gmail.com>
2024-10-25 09:31:23 -0300

    Juntei o finishEvents em uma função só

--- src/events/finishEvent.ts
+++ src/events/finishEvent.ts
@@ -4,12 +4,12 @@ import dotenv from "dotenv";
 import { error } from "console";
 dotenv.config();
 
-async function getEventPool(IdEvt: number) {
+async function finishEvent(IdEvt: number,ResultadoEvento:string) {
     OracleDB.outFormat = OracleDB.OUT_FORMAT_OBJECT;
     let connection;
 
     let pool:number = 0;
-
+    let winnerPool:number = 0;
     try{
         connection = await OracleDB.getConnection({
             user: process.env.ORACLE_USER,
@@ -60,46 +60,6 @@ async function getEventPool(IdEvt: number) {
         }else{
             throw new Error("Nenhuma linha encontrada.");
         }
-    }catch(err){
-        console.log('DataBase error',err);
-        throw new Error("Error during geting event Pool.");
-    }finally{
-        if (connection){
-            try{
-                await connection.close();
-            } catch (err) {
-                console.error("Error closing the connection: ", err);
-            }
-        }
-    }
-
-    return pool;
-}
-
-async function getWinnerGamblers(IdEvt: number,ResultadoEvento: string){
-    OracleDB.outFormat = OracleDB.OUT_FORMAT_OBJECT;
-    let connection;
-
-    let winnerPool:number = 0;
-
-    try{
-        connection = await OracleDB.getConnection({
-            user: process.env.ORACLE_USER,
-            password: process.env.ORACLE_PASSWORD,
-            connectString: process.env.ORACLE_CONN_STR
-        });
-      
-        interface Bet {
-            ID_APT: number;
-            QTD_COTAS: number;
-            FK_ID_EVT: number;
-            FK_ID_USR: number;
-            ESCOLHA: string;
-        }
-
-        interface valorCotasResult{
-            VALOR_COTA: number;
-        }
 
         const allWinnersBets = await connection.execute<Bet>(
             `SELECT *
@@ -108,18 +68,6 @@ async function getWinnerGamblers(IdEvt: number,ResultadoEvento: string){
             [IdEvt, ResultadoEvento],
             { outFormat: OracleDB.OUT_FORMAT_OBJECT }
         );
-
-        let resultValorCota = await connection.execute<valorCotasResult>(
-            `SELECT VALOR_COTA
-            FROM EVENTS
-            WHERE ID_EVT = :IdEvt`,
-            {IdEvt}
-        );
-
-        let valorCota = resultValorCota.rows?.[0]?.VALOR_COTA;
-        if (!valorCota) {
-            throw new Error("Evento com parametro faltante.");
-        };
 
         if(allWinnersBets.rows && allWinnersBets.rows.length > 0){
             for(const row of allWinnersBets.rows){
@@ -130,64 +78,6 @@ async function getWinnerGamblers(IdEvt: number,ResultadoEvento: string){
         }else{
             throw new Error("Nenhuma linha encontrada.");
         }
-    }catch(err){
-        console.log('DataBase error',err);
-        throw new Error("Error during geting event WinnerPool.");
-    }finally{
-        if (connection){
-            try{
-                await connection.close();
-            } catch (err) {
-                console.error("Error closing the connection: ", err);
-            }
-        }
-    }
-
-    return winnerPool;
-}
-
-async function distributePool(IdEvt: number,ResultadoEvento: string,pool: number,winnersPool: number) {
-    OracleDB.outFormat = OracleDB.OUT_FORMAT_OBJECT;
-    let connection;
-
-    try{
-        connection = await OracleDB.getConnection({
-            user: process.env.ORACLE_USER,
-            password: process.env.ORACLE_PASSWORD,
-            connectString: process.env.ORACLE_CONN_STR
-        });
-      
-        interface Bet {
-            ID_APT: number;
-            QTD_COTAS: number;
-            FK_ID_EVT: number;
-            FK_ID_USR: number;
-            ESCOLHA: string;
-        }
-
-        interface valorCotasResult{
-            VALOR_COTA: number;
-        }
-
-        const allWinnersBets = await connection.execute<Bet>(
-            `SELECT *
-            FROM BETS
-            WHERE FK_ID_EVT = :IdEvt AND ESCOLHA = :ResultadoEvento`,
-            [IdEvt, ResultadoEvento],
-            { outFormat: OracleDB.OUT_FORMAT_OBJECT }
-        );
-
-        let resultValorCota = await connection.execute<valorCotasResult>(
-            `SELECT VALOR_COTA
-            FROM EVENTS
-            WHERE ID_EVT = :IdEvt`,
-            {IdEvt},
-        );
-
-        let valorCota = resultValorCota.rows?.[0]?.VALOR_COTA;
-        if (!valorCota) {
-            throw new Error("Evento com parametro faltante.");
-        };
 
         if(allWinnersBets.rows && allWinnersBets.rows.length > 0){
             for(const row of allWinnersBets.rows){
@@ -213,7 +103,6 @@ async function distributePool(IdEvt: number,ResultadoEvento: string,pool: number
                 if (!idCrt) {
                     throw new Error("Usuário com este id.");
                 };
-
                 let resultBalance = await connection.execute<balanceResult>(
                     `SELECT SALDO
                      FROM WALLETS
@@ -227,7 +116,7 @@ async function distributePool(IdEvt: number,ResultadoEvento: string,pool: number
                 }
 
                 let valorAposta = valorCota * QTD_COTAS;
-                let proportion = valorAposta/winnersPool;
+                let proportion = valorAposta/winnerPool;
                 let newBalance = pool * proportion;
                 balance += newBalance;
 
@@ -246,9 +135,10 @@ async function distributePool(IdEvt: number,ResultadoEvento: string,pool: number
         }else{
             throw new Error("Nenhuma linha encontrada.");
         }
+
     }catch(err){
         console.log('DataBase error',err);
-        throw new Error("Error during distributing pool.");
+        throw new Error("Error during geting event Pool.");
     }finally{
         if (connection){
             try{
@@ -259,6 +149,7 @@ async function distributePool(IdEvt: number,ResultadoEvento: string,pool: number
         }
     }
 }
+ 
 export const finishEventHandler: RequestHandler = async (req : Request, res : Response) => {
     const pIdAdmin = Number(req.get('id_admin'));
     const pStatus = req.get('status');
@@ -266,9 +157,7 @@ export const finishEventHandler: RequestHandler = async (req : Request, res : Re
     const pResultadoEvento = req.get('resultado')?.toLowerCase();
 
     if(!isNaN(pIdAdmin) && pStatus && !isNaN(pIdEvt) && pResultadoEvento){
-        const pool = await getEventPool(pIdEvt);
-        const winnersPool = await getWinnerGamblers(pIdEvt,pResultadoEvento);
-        await distributePool(pIdEvt,pResultadoEvento,pool,winnersPool);
+        await finishEvent(pIdEvt,pResultadoEvento);
         res.statusCode = 200;
         res.send('Evento finalizado com sucesso.');
     }else{
